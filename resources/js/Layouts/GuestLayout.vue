01de6313640bef472f08af0967dca48e6150bb5d
Syed Danish Kazmi <s.danish.kazmi555@gmail.com>
2024-05-22 15:59:15 +0500

    Update auth

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -8,7 +8,7 @@ import { Link } from '@inertiajs/vue3';
         <!-- Left part with white background -->
         <div class="flex justify-center items-center w-full sm:w-1/2 mx-6">
             <!-- Your login content goes here -->
-            <div class="w-full sm:max-w-md my-6 px-6 pt-8 pb-12 bg-white shadow-md overflow-hidden rounded-lg">
+            <div class="w-full sm:max-w-lg my-6 px-6 pt-8 pb-12 bg-white shadow-md overflow-hidden rounded-lg">
                 <div class="flex flex-col sm:justify-center items-center mb-6">
                     <Link href="/">
                         <ApplicationLogo class="w-20 h-20 fill-current text-gray-500" />
